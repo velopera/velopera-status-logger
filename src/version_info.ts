66e6b981e5cfabe00b0ed1e0b51e414bcd93b948
Ferman Guzel <fguzel@voxel.at>
2025-04-10 14:32:29 +0200

    implemented eslint and git_version control

---
+++ src/version_info.ts
@@ -0,0 +1,8 @@
+export const version = {
+  tag: 'v1.0.0',
+  raw: 'v1.0.0-0-ge35928c-dirty',
+  Commit: 'e35928cc594a844457d315077981ad4d4a55f9a1',
+  Author: 'Merge',
+  Date: '62a9df9 a667a52',
+  Message: 'Ferman <162349104+ferman-guzel@users.noreply.github.com>'
+}
