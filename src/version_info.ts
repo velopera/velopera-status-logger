c1319b8aa6e3799278b88651db79fd91eaa71976
Ferman Guzel <fguzel@voxel.at>
2025-05-13 09:16:24 +0200

    npm updated

--- src/version_info.ts
+++ src/version_info.ts
@@ -1,6 +1,6 @@
 export const version = {
   tag: 'v1.0.0',
-  raw: 'v1.0.0-26-gb18b6c0',
+  raw: 'v1.0.0-37-g4e6ff4a',
   Commit: 'e35928cc594a844457d315077981ad4d4a55f9a1',
   Author: 'Merge',
   Date: '62a9df9 a667a52',
